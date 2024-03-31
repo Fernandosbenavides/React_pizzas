68d4286c82efbe1664a12ddc27623af9288f515f
fernandosbenavides <fernandosbenavides@gmail.com>
2024-03-31 18:15:05 -0300

    primera version

---
+++ src/views/NotFound.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div>
+      <img src="src\assets\img\404.webp" alt="" />
+      <h1 className="mt-5 text-danger">404</h1>
+      <h2 className="">No encontramos la pagina que buscabas</h2>
+    </div>
+  );
+};
+
+export default NotFound;
